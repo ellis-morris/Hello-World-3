cee1e2cf55aa3fce88d83f6963c1cdbce6fefe2d
Ellis Morris <RC0044018@student.riversidecollege.ac.uk>
2020-12-14 14:58:29 +0000

    Added Hello Button

--- Hello World 3/Form1.cs
+++ Hello World 3/Form1.cs
@@ -19,7 +19,7 @@ namespace Hello_World_3
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World");
+            MessageBox.Show("Hello " + txtBxName.Text);
         }
     }
 }
